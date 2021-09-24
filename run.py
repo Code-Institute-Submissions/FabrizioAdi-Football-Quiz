fc59fcd6d2fea0974981bad72c6b03231dc2076c
FabrizioAdi <adriannoszczyk@gmail.com>
2021-09-24 13:06:42 +0000

    Create dictionary call question and list of answers

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# ********************
+questions = {
+    "Who is the winner of Premier League (England) 2020/21?", "correct answer":"a",
+    "What football country does lionel messi represent?", "correct answer":"c",
+    "Who broke Gerd Müller's record for the most goals scored in a single Bundesliga season?", "correct answer":"a",
+
+}
+answers = [["a":"Manchester City", "b":"Manchester United", "c":"Chelsea London", "d":"Leicester City"],
+["a":"Italy", "b":"Uruguay", "c":"Argentina", "d":"USA"],
+["a":"Robert Lewandowski (Pol)", "b":"Gerd Muller (Ger)", "c":"Cristiano Ronaldo (Por)", "d":"Thomas Muller (Ger)"]]
+
+# ********************
+# ********************
