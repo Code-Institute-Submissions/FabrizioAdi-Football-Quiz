7e36888d0f6845564c684bb2f2dd78007eb5d0a5
FabrizioAdi <adriannoszczyk@gmail.com>
2021-09-24 15:00:15 +0000

    Add more quiz questions and answers

--- run.py
+++ run.py
@@ -14,16 +14,28 @@ def run_game():
         for i in answers[question_number-1]:
             print(i)
 
+        question_number += 1
+
 
 questions = {
     "Who is the winner of Premier League (England) 2020/21?":"a",
-    "What football country does lionel messi represent?":"c",
+    "Which international club does Lionel Messi play for?":"c",
     "Who broke Gerd Müller's record for the most goals scored in a single Bundesliga season?":"a",
+    "What international team play in Wembley?":"a",
+    "Which of these teams is from Turin?":"d",
+    "How long is a game of professional football?":"b",
+    "How many times have Brazil won the World Cup?":"d",
+    "Which international club does Robert Lewandowski play for?":"a",
 
 }
 answers = [["a. Manchester City", "b. Manchester United", "c. Chelsea London", "d. Leicester City"],
 ["a. Italy", "b. Uruguay", "c. Argentina", "d. USA"],
-["a. Robert Lewandowski (Pol)", "b. Gerd Muller (Ger)", "c. Cristiano Ronaldo (Por)", "d. Thomas Muller (Ger)"]]
+["a. Robert Lewandowski (Pol)", "b. Gerd Muller (Ger)", "c. Cristiano Ronaldo (Por)", "d. Thomas Muller (Ger)"],
+["a. England", "b. Denmark", "c. Sweden", "d. Scotland"],
+["a. AC Milan", "b. SSC Napoli", "c. Atletico", "d. Juventus"],
+["a. 60 minutes", "b. 90 minutes", "c. 75 minutes", "d. 45 minutes"],
+["a. Three times", "b. Four times", "c. Six times", "d. Five times"],
+["a. Poland", "b. Russia", "c. Italy", "d. France"]]
 
 # ********************
 # ********************
